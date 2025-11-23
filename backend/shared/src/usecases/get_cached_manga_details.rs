3d089f956bf84ef231ee572e1dcd9f325cc1b8cf
Tachibana Shin <tachibshin@duck.com>
2025-11-23 22:32:00 +0700

    fix(manga): refactor URL handling in get_cached_manga_details function

--- backend/shared/src/usecases/get_cached_manga_details.rs
+++ backend/shared/src/usecases/get_cached_manga_details.rs
@@ -11,14 +11,21 @@ pub async fn get_cached_manga_details(
     match db.find_cached_manga_details(&id).await {
         Some((mut details, per_read)) => {
             if let Some(url) = &details.cover_url {
-                details.url = Some(
-                    url::Url::from_file_path(
-                        chapter_storage
-                            .cached_poster(&url, || source.get_image_request(url.to_owned()))
-                            .await?,
-                    )
-                    .map_err(|_| url::ParseError::IdnaError)?,
-                );
+                let req = source.get_image_request(url.to_owned()).await?;
+
+                let output = chapter_storage
+                    .cached_poster(&url, || source.get_image_request(url.to_owned()))
+                    .await?
+                    .canonicalize()?;
+
+                details.url = url::Url::from_file_path(output)
+                    .map_err(|_| {
+                        println!(
+                            "Error converting path to URL: {:?}",
+                            url::ParseError::IdnaError
+                        );
+                    })
+                    .ok();
             }
 
             Ok(Some((details, per_read)))

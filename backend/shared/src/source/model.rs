b498d2cf6cd613cc160cd8868d5201b9422b69f2
Tachibana Shin <tachibshin@duck.com>
2026-05-15 17:34:07 +0700

    feat: add picker setting type alias for select
    https://github.com/Aidoku/aidoku-rs/commit/50740dc4f9c286a9e747ba10c2e6594118052397

--- backend/shared/src/source/model.rs
+++ backend/shared/src/source/model.rs
@@ -20,7 +20,8 @@ pub enum SettingDefinition {
     },
     #[serde(rename = "select")]
     // `segment` works just like a `select`, but it's shown as a segmented button in Aidoku.
-    #[serde(alias = "segment")]
+    // `picker` is an inline picker that allows selection of a single value.
+    #[serde(alias = "segment", alias = "picker")]
     Select {
         title: String,
         key: String,

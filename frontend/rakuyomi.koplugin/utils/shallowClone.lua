febad8942ffe0c4713cd0fd69211a1b85d8d375e
Tachibana Shin <tachibshin@duck.com>
2026-07-13 23:49:06 +0700

    Update shallowClone.lua

--- frontend/rakuyomi.koplugin/utils/shallowClone.lua
+++ frontend/rakuyomi.koplugin/utils/shallowClone.lua
@@ -1,9 +1,18 @@
-local function shallow_clone(arr)
-    local clone = {}
-    for i = 1, #arr do
-        clone[i] = arr[i]
+local table_move = table.move
+if not table_move then
+    table_move = function(a1, f, e, t, a2)
+        a2 = a2 or a1
+        if e >= f then
+            for i = 0, e - f do
+                a2[t + i] = a1[f + i]
+            end
+        end
+        return a2
     end
-    return clone
+end
+
+local function shallow_clone(arr)
+    return table_move(arr, 1, `#arr`, 1, {})
 end
 
 return shallow_clone

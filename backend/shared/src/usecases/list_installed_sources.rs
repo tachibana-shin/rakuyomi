3b28fde5c5d6d8943b6735326fabfaeb529c6916
BaconDroid <911ben@gmail.com>
2026-07-19 17:42:32 -0400

    Update list_installed_sources.rs

--- backend/shared/src/usecases/list_installed_sources.rs
+++ backend/shared/src/usecases/list_installed_sources.rs
@@ -4,7 +4,17 @@ pub fn list_installed_sources(source_collection: &impl SourceCollection) -> Vec<
     let mut source_informations: Vec<SourceInformation> = source_collection
         .sources()
         .into_iter()
-        .map(|source| source.manifest().into())
+        .map(|source| {
+            let manifest_info: SourceInformation = source.manifest().into();
+            SourceInformation {
+                supported_sort_buckets: source
+                    .supported_sort_buckets()
+                    .into_iter()
+                    .map(|b| format!("{:?}", b).to_lowercase())
+                    .collect(),
+                ..manifest_info
+            }
+        })
         .collect();
 
     source_informations.sort_by_key(|source| source.name.clone());

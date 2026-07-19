957272b57d7b2b109aaf50fbaf0121783569ceb0
Tachibana Shin <shin@shin.is-a.dev>
2026-07-19 11:22:20 +0700

    Update SettingItemValue.lua

--- frontend/rakuyomi.koplugin/widgets/SettingItemValue.lua
+++ frontend/rakuyomi.koplugin/widgets/SettingItemValue.lua
@@ -357,9 +357,6 @@ function SettingItemValue:onTap()
     UIManager:show(path_chooser)
   end
 
-  -- Consume the tap: otherwise it keeps bubbling up to the containing view,
-  -- whose top-zone gesture handler may open the KOReader menu on top of the
-  -- dialog we just showed.
   return true
 end
 

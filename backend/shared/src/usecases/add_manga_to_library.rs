808703e4bb08a1cce63bc1c48d7dcfeca3e5d82b
BaconDroid <911ben@gmail.com>
2026-07-20 09:10:37 -0400

    Update add_manga_to_library.rs

--- backend/shared/src/usecases/add_manga_to_library.rs
+++ backend/shared/src/usecases/add_manga_to_library.rs
@@ -2,11 +2,7 @@ use anyhow::Result;
 use tokio::time::{timeout, Duration};
 use tokio_util::sync::CancellationToken;
 
-use crate::{
-    database::Database,
-    model::{MangaId, MangaInformation},
-    source::Source,
-};
+use crate::{database::Database, model::MangaId, source::Source};
 
 pub async fn add_manga_to_library(
     token: &CancellationToken,
@@ -20,9 +16,7 @@ pub async fn add_manga_to_library(
         let fetch_task = source.get_manga_details(child_token.clone(), id.value().clone());
 
         if let Ok(Ok(manga)) = timeout(Duration::from_secs(seconds), fetch_task).await {
-            let _ = db
-                .upsert_cached_manga_information(&[MangaInformation::from(manga)])
-                .await;
+            let _ = db.upsert_cached_manga_information(&[manga.information]).await;
         }
     }
 

7e0b5967ed9da9699c07ef08d0cd02e9a771b1f6
BaconDroid <911ben@gmail.com>
2026-07-20 13:09:13 -0400

    Update add_manga_to_library.rs

--- backend/shared/src/usecases/add_manga_to_library.rs
+++ backend/shared/src/usecases/add_manga_to_library.rs
@@ -19,10 +19,14 @@ pub async fn add_manga_to_library(
         let child_token = token.child_token();
         let fetch_task = source.get_manga_details(child_token.clone(), id.value().clone());
 
-        if let Ok(Ok(manga)) = timeout(Duration::from_secs(seconds), fetch_task).await {
-            let _ = db
-                .upsert_cached_manga_information(&[MangaInformation::from(manga)])
-                .await;
+        match timeout(Duration::from_secs(seconds), fetch_task).await {
+            Ok(Ok(manga)) => {
+                let _ = db
+                    .upsert_cached_manga_information(&[MangaInformation::from(manga)])
+                    .await;
+            }
+            Err(_) => child_token.cancel(),
+            Ok(Err(_)) => {}
         }
     }
 
